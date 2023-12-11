2b8e97a66aa2bada69088fd8e653cba3206e3f1f
SalvaJuega <88589081+salvajuegaYT@users.noreply.github.com>
2023-12-11 15:42:49 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,21 @@
+function Ir(1) {
+  window.location.href = "1.html";
+}
+function Ir(2) {
+  window.location.href = "2.html";
+}
+function Ir(3) {
+  window.location.href = "3.html";
+}
+function Ir(4) {
+  window.location.href = "4.html";
+}
+function Ir(5) {
+  window.location.href = "5.html";
+}
+function Ir(6) {
+  window.location.href = "6.html";
+}
+function Ir(7) {
+  window.location.href = "7.html";
+}
